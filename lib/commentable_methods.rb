7fde9b0ed6587cbb4ee888a8990d949186c758a1
tranquangvu <vutran.513@gmail.com>
2023-04-20 17:56:11 +0700

    Allow ruby 3.x can be work

--- lib/commentable_methods.rb
+++ lib/commentable_methods.rb
@@ -17,20 +17,21 @@ module Juixe
 
         def define_role_based_inflection_3(role)
           has_many "#{role.to_s}_comments".to_sym,
-                   has_many_options(role).merge(:conditions => { role: role.to_s })
+                   **has_many_options(role).merge(conditions: { role: role.to_s })
         end
 
         def define_role_based_inflection_4(role)
           has_many "#{role.to_s}_comments".to_sym,
                    -> { where(role: role.to_s) },
-                   has_many_options(role)
+                   **has_many_options(role)
         end
 
         def has_many_options(role)
-          {:class_name => "Comment",
-                  :as => :commentable,
-                  :dependent => :destroy,
-                  :before_add => Proc.new { |x, c| c.role = role.to_s }
+          {
+            class_name: 'Comment',
+            as: :commentable,
+            dependent: :destroy,
+            before_add: Proc.new { |x, c| c.role = role.to_s }
           }
         end
       end
@@ -53,9 +54,9 @@ module Juixe
             comment_roles.each do |role|
               define_role_based_inflection(role)
             end
-            has_many :all_comments, { :as => :commentable, :dependent => :destroy, class_name: 'Comment' }.merge(join_options)
+            has_many :all_comments, **({ as: :commentable, dependent: :destroy, class_name: 'Comment' }.merge(join_options))
           else
-            has_many :comments, {:as => :commentable, :dependent => :destroy}.merge(join_options)
+            has_many :comments, **({as: :commentable, dependent: :destroy}.merge(join_options))
           end
 
           comment_types.each do |role|
